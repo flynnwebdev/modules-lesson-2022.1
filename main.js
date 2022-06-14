8f9bbb330251b6f97a5248ffef627d0bbccbc2ed
Flynn <flynnwebdev@gmail.com>
2022-06-14 14:14:34 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import { add } from './math.js'
+import random from "./node_modules/lodash-es/random.js"
+
+// console.log(num)
+// console.log(add(5,10))
+console.log(random(10, 99))
---
+++ math.js
@@ -0,0 +1,13 @@
+const PI = 3.14159
+
+const num = 1234
+
+function add(x, y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+export { PI, add, subtract }
